fbc6a9587937aae2d70efae39946bb8fae198a0d
Muhammad Raffiq Mohd Khalil <muhammadraffiq@gmail.com>
2020-10-13 13:49:44 +0800

    Update Picture.java
    Updated the get and setter

--- Backend/phoblock/src/main/java/com/hackathon/phoblock/Model/Picture.java
+++ Backend/phoblock/src/main/java/com/hackathon/phoblock/Model/Picture.java
@@ -11,5 +11,82 @@ public class Picture {
     @Column(nullable = false)
     String pictureCaption;
     @Column(nullable = false)
-    PhoBlockUser picture_owner;
+    PhoBlockUser pictureOwner;
+    @Column(nullable = false)
+    String pictureTag;
+    @Column(nullable = false)
+    String pictureTheme; 
+    @Column (nullable = true)
+    Integer numberDownload;
+    @Column (nullable = true)
+    Integer numberFavorite;
+    @Column (nullable = false);
+    String product;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    Date dateCreated;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getPictureCaption() {
+        return pictureCaption;
+    }
+
+    public String getPictureOwner() {
+        return pictureOwner;
+    }
+
+    public String getPictureTag() {
+        return pictureTag;
+    }
+
+    public String getPictureTheme() {
+        return pictureTheme;
+    }
+
+    public Integer getNumberDownload() {
+        return numberDownload;
+    }
+
+    public Integer getNumberFavorite() {
+        return numberFavorite;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setPictureCaption(String pictureCaption) {
+        this.pictureCaption = pictureCaption;
+    }
+
+    public void setPictureOwner(String pictureOwner) {
+        this.pictureOwner = pictureOwner;
+    }
+
+    public void setPictureTag(String pictureTag) {
+        this.pictureTag = pictureTag;
+    }
+
+    public void setPictureTheme(String pictureTheme) {
+        this.pictureTheme = pictureTheme;
+    }
+
+    public void setNumberDownload(Integer numberDownload) {
+        this.numberDownload = numberDownload;
+    }
+
+    public void setNumberFavorite(Integer numberFavorite) {
+        this.numberFavorite = numberFavorite;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
 }
